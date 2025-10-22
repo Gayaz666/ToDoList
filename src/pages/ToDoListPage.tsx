33680b415f7c8705eddde76f181bf6266d8b69dd
Gayaz Urazov <gayazur@mail.ru>
2025-10-22 10:05:41 +0500

    third lesson

--- src/pages/ToDoListPage.tsx
+++ src/pages/ToDoListPage.tsx
@@ -1,13 +1,58 @@
+import { useState } from "react"
 import { Form } from "../components/Form/Form"
 import { Header } from "../components/Header/Header"
 import { ToDoList } from "../components/ToDoList/ToDoList"
+import { ToDo } from "../models/todo-item"
+import React from 'react'
+import { Bounce, ToastContainer, toast } from 'react-toastify'
 
 export const ToDoListPage = () => {
+    const [todos, setTodos] = useState<ToDo[]>([])
+
+    const createNewToDo = (text: string) => {
+        const newToDo: ToDo = {
+            id: todos.length,
+            text: text,
+            isDone: false
+        }
+        setTodos([...todos, newToDo])
+        toast.info('create new ToDo item');
+    }
+
+    const updateToDo = (toDoItem: ToDo) => {
+        const newTodos = todos.map((todo) => {
+            if (todo.id === toDoItem.id) {
+                todo.isDone = !todo.isDone
+            }
+            return todo
+        })
+        setTodos(newTodos)
+        toast.info('update ToDo item');
+    }
+
+    const deleteToDo = (toDoItem: ToDo) => {
+        const newTodos = todos.filter((todo) => todo.id !== toDoItem.id)
+        setTodos(newTodos)
+        toast.info('delete ToDo item');
+    }
     return (
         <>
             <Header />
-            <Form />
-            <ToDoList />
+            <Form createNewToDo={createNewToDo} />
+            <ToDoList todos={todos} updateToDo={updateToDo} deleteToDo={deleteToDo} />
+            <ToastContainer
+                position="bottom-right"
+                autoClose={5000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick={false}
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+                transition={Bounce}
+            />
         </>
     )
 }
